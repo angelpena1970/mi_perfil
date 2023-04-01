afa41b679042b2d2ffe5cf82e747e3ac449d54ca
angelpena1970 <118632698+angelpena1970@users.noreply.github.com>
2023-04-01 18:35:33 +0200

    Add files via upload

---
+++ bienvenida.py
@@ -0,0 +1,19 @@
+def mostrar_mensaje():
+    nombre = input("Por favor, ingresa tu nombre: ")
+
+    mensaje = f"""¡Hola, {nombre}! Gracias por visitar mi perfil en GitHub.
+
+Mi nombre es Angel Peña Pérez y soy un abogado de personas refugiadas apasionado por el mundo del social impact blockchain & Machine Learning. A lo largo de mi carrera, he trabajado en diversos proyectos interesantes.
+
+Si deseas obtener más información sobre mi trabajo o si estás interesado en colaborar en un proyecto, no dudes en contactarme.
+
+Correo electrónico: angel@ecode.es
+LinkedIn: /in/angel1970/
+
+¡Espero saber de ti pronto!"""
+
+    print(mensaje)
+
+
+if __name__ == "__main__":
+    mostrar_mensaje()
